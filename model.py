8293524505a41a75f9d2a500bc7ee8a2b8202abb
Shaoqing Tan <tansq7@gmail.com>
2019-12-04 14:07:00 -0500

    WIP

--- model.py
+++ model.py
@@ -1,12 +1,26 @@
 import yaml
-from pprint import pprint as pp
+import pprint
+
+class O(object):
+  def __repr__(self):
+    d = {key: val for key, val in self.__dict__.items() if key[0] != '_'}
+    return pprint.pformat(d, indent=2)
+
+def objectize(items):
+  root = O()
+  setattr(root, 'items', items)
+  setattr(root, 'index', O())
+  for itemIdx, item in enumerate(items):
+    setattr(root.index, item['name'], itemIdx)
+    attr = O()
+    for idx, value in enumerate(item['values']):
+      setattr(attr, value, idx)
+    setattr(root, item['name'], attr)
+  return root
 
 CONST = yaml.safe_load(open('constants.yaml'))
-
-STATE = CONST['STATE']
-
+STATE = objectize(CONST['STATE'])
+ACTION = objectize(CONST['ACTION'])
 
 def first_state():
-  return 
-
-def tw_model(state, )
+  return [0] * len(STATE.items)
